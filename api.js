a76c53649be5884b9159e77d01d87e31d0ff530d
Bikram Singh <bikramjeet.singh@hotmail.com>
2023-04-02 09:59:57 -0500

    misx

--- api.js
+++ api.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export const getSummary = async (transcript) => {
   console.log("TRANS " + transcript);
-  const prompt = `Give me a summarized version of this in numerical points. It must only contain the important points from the conversation -  ${transcript}`;
+  const prompt = `Give me a summarized version of this in numerical points that we need to remember -   ${transcript}`;
   const data = {
     model: "text-davinci-003",
     prompt: prompt,
     max_tokens: 4000,
-    temperature: 0.6,
+    temperature: 0.4,
   };
   return await axios
     .post("https://api.openai.com/v1/completions", data, {
       headers: {
         "Content-Type": "application/json",
         Authorization:
-          "Bearer  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",
+          "Bearer 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",
       },
     })
     .then((res) => {
--- components/SummaryModal.jsx
+++ components/SummaryModal.jsx
@@ -9,7 +9,7 @@ function SummaryModal(props) {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className=" flex flex-col backdrop-blur-lg bg-white h-auto w-4/6 px-4 py-4 rounded-lg">
+        <div className=" flex flex-col backdrop-blur-lg bg-white/80 h-auto w-4/6 md:w-1/3 px-4 md:px-10 md:py-10  py-8 rounded-lg">
           <div
             className="flex justify-end "
             onClick={() => setSummaryModalActive(false)}
@@ -29,9 +29,14 @@ function SummaryModal(props) {
               />
             </svg>
           </div>
+          <div>Summary based on conversation - </div>
           {/* SUMMARY DATA BELOW */}
           {data.map((point) => {
-            return <div className="text-[12px] text-bold my-2">{point}</div>;
+            return (
+              <div className="md:text-[18px] text-[14px] font-bold my-2">
+                {point}
+              </div>
+            );
           })}
         </div>
       </div>
